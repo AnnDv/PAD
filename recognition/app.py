666541de4a4f90f8d25e2932c0a4d49de01831b9
Lina Chipciu <linachipciu@MacBook-Pro-Lina.local>
2022-10-11 14:31:39 +0300

    add recognition

---
+++ recognition/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    user_id = request_data['user_id']
+    phrase = request_data['phrase']
+
+    return Response (
+        status=200
+    )
+ 
+# main driver function
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
