63d4f19648540ebdbfcdf017ece71bd4b6b474d0
Lina Chipciu <linachipciu@MacBook-Pro-Lina.local>
2022-11-14 20:17:22 +0200

    Connect MongoDB

--- history/server.js
+++ history/server.js
@@ -20,19 +20,33 @@ const port = 3030;
 
 app.use(express.json());
 
-app.post('/history', (req, res, next) => {
-  var userData = new History(req.body);
-  userData.save()
-    .then(item => {
-      console.log(req.body)
-      res.send("item saved to database");
-    })
-    .catch(err => {
-      res.status(400).send("unable to save to database");
-    });
+app.post('/history', async (req, res, next) => {
+    // var userData = new History(req.body);
+    // userData.save()
+    //   .then(item => {
+    //     console.log(req.body['user'])
+    //     res.send("item saved to database");
+    //   })
+    //   .catch(err => {
+    //     res.status(400).send("unable to save to database");
+    //   });
     
-    var query = History.find({}).select("user_id");
-    console.log(query)
+    let id = req.body['user_id']
+   let history = await History.findOne({ 'user_id': id }).exec();
+    if (history == null) {
+      userData = new History(req.body).save()
+    } else { 
+      console.log(history)
+      let movies = req.body['movies']
+      for (let elm of movies) {
+      console.log(elm);
+      if (history.movies.includes(elm) == false){
+        history.movies.push(elm)
+      }
+    }
+    history.save()
+    }
+    res.status(200).send("ok")
 })
 
 // app.get('/history', (req, res) => {
